3c13520092415b831c08c1fa3cc7dfbc41e5d4ce
Gegerastenis <giselle_rastenis@hotmail.com>
2024-12-06 18:13:02 -0300

    modificando Ejercicio 9

--- Clase13/Ejercicio9.js
+++ Clase13/Ejercicio9.js
@@ -2,11 +2,10 @@
 Crea una función declarada llamada contarHasta que reciba un número y 
 use un bucle para imprimir todos los números desde 1 hasta ese número.*/
 const prompt = require("prompt-sync")({ sigint: true})
-let ingreseunNumero = parseInt(prompt("Por favor ingrese un numero: "));
+let numero = parseInt(prompt("Por favor ingrese un numero: "));
 function contarHasta(numero){
-for(let i = 1; i < numero; i++){
+for(let i = 1; i <= numero; i++){
     console.log(i);
   }
-return (numero);
 }
-console.log(contarHasta(ingreseunNumero));
+contarHasta(numero);
