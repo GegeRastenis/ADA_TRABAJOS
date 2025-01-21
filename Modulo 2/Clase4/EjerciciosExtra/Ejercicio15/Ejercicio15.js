2c3a9f93ca497bc680d579c947ef25df0f2ba7f9
Gegerastenis <giselle_rastenis@hotmail.com>
2025-01-21 10:52:42 -0300

    Subiendo Ejercicio15.js a EjerciciosExtra

--- Modulo 2/Clase4/EjerciciosExtra/Ejercicio15/Ejercicio15.js
+++ Modulo 2/Clase4/EjerciciosExtra/Ejercicio15/Ejercicio15.js
@@ -8,3 +8,49 @@ minutos) y fecha.
 3. Filtrar actividades por tipo (por ejemplo, solo mostrar las de 
 "correr").
  Pista: Usa Date.now() para obtener la fecha exacta*/
+
+ const fs = require('fs')
+ const filePath = './actividades.json'
+
+ const leerActividades = () => {
+       if(fs.existsSync(filePath)){
+               const data = fs.readFileSync(filePath, 'utf-8')
+               return JSON.parse(data)
+           }
+           return []
+   }
+   const guardarActividades = (actividades) => {
+       fs.writeFileSync(filePath, JSON.stringify(actividades, null, 2))
+   }
+
+   const registrarActividades = (tipo, duracionMinutos)=>{
+    const actividades = leerActividades()
+    const fecha = new Date(Date.now()).toLocaleDateString()
+    actividades.push({tipo, duracionMinutos, fecha})
+    guardarActividades(actividades)
+    console.log(`La actividad ${tipo} ha sido ingresada exitosamente`)
+   }
+
+   //registrarActividades('correr', 50)
+   //registrarActividades('nadar', 30)
+   //registrarActividades('andar en bici', 120)
+   //registrarActividades('pole sport', 60)
+
+   const listarActividades = ()=>{
+    const actividades = leerActividades()
+    //actividades.forEach(actividad =>{
+    //console.log(`${actividad.tipo}: ${actividad.duracionMinutos}, ${actividad.fecha} `)
+     // })
+    console.log("Lista de actividades: ")
+    console.table(actividades)
+   }
+   //listarActividades()
+
+   const buscarActividad = (tipo)=>{
+   const actividades = leerActividades()
+   const actividadEncontrada = actividades.filter(actividad => actividad.tipo === tipo)
+   console.log('La actividad buscada es: ')
+   console.log(actividadEncontrada)
+   }
+
+   buscarActividad('nadar')
