78c77a2f4ca51724be9a6f0436037ee73bfaf777
Gegerastenis <giselle_rastenis@hotmail.com>
2025-03-06 17:55:20 -0300

    Subiendo Clase14

--- Modulo 2/Clase14/Ejercicio6.js
+++ Modulo 2/Clase14/Ejercicio6.js
@@ -3,3 +3,29 @@ Crea un script en Node.js que permita al usuario ingresar una contraseña
 y un "sal" (un valor aleatorio que se usa para añadir seguridad). Luego, 
 genera un hash de la contraseña combinada con el "sal" usando el 
 algoritmo SHA-256. Muestra el hash generado*/
+
+const crypto = require('crypto')
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout
+}); 
+
+function hashPasswordAndSalt (password, salt){
+const hash = crypto.createHash('sha256'); 
+hash.update(password, salt); 
+return hash.digest('hex'); 
+}; 
+
+rl.question('Por favor ingrese su contrasenia: ', (password)=>{
+    rl.question('Por favor ingrese el "sal": ', (salt)=>{
+        const hashGenerado = hashPasswordAndSalt(password, salt);
+
+        console.log('El Hash generado es: ', hashGenerado); 
+
+        rl.close(); 
+    })
+})
+
+    
